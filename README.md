86f4cd12a337498cd9334873e629fa3a41a9e7b2
Jay L. Johnson <jay@oeconomicas.com>
2021-05-08 10:00:03 -0700

    automate vscode ssh config, and directly connect via ssh

--- README.md
+++ README.md
@@ -1,5 +1,28 @@
 # automation-scripts
 
-## Utility scripts for automation
-
 No warranty is provided, use at your own risk!
+
+## Setup
+Prerequisites: 
+1. An EC2 host in running or stopped state
+2. A .pem file granting access to the remote ec2 host
+
+Local setup:
+1. Copy the local.env.template `cp local.env.template local.env`
+2. Populate the local.env with your EC2 instance and .pem details
+3. Run `source local.env`
+
+## Usage
+### /aws/ec2-start/instance.sh
+Prerequisites: 
+1. README.md setup steps are complete
+2. VSCode running on a windows machine
+3. Windows with wsl2
+4. An EC2 host in running or stopped state
+
+What this script does:
+1. Startup an EC2 instance id
+2. Grant security group access to the local IP address
+3. Update the VS Code remote ssh config file with the EC2 dns name
+4. Establish a SSH connection to the EC2 host in the current terminal
+
--- aws/ec2-start-instance.sh
+++ aws/ec2-start-instance.sh
@@ -13,15 +13,34 @@
 # 	Example:
 # 	INSTANCE_ID=i-0e4889b229035e771 SECURITY_GROUP_ID=sg-065c9be5359a994e8 ./ec2-start-instance.sh
 #
+#   VSCODE_SSH_CONFIG_PATH="/mnt/c/Users/Jay/.ssh/config"
+#   VSCODE_SSH_PEM_FILE_PATH="C:\Users\Jay\OneDrive\Documents\dev-gl\default-remote-dev.pem"
+#   SSH_PEM_FILE_PATH="~/aws/default-remote-dev.pem"
 ######
 
 aws ec2 start-instances --instance-ids $INSTANCE_ID
 
+# Authorize the client's ip to access the ec2 host's security group
 IP=`curl -s http://whatismyip.akamai.com/`
-echo "Updating the security group to my current ip: $IP"
+echo "Updating the security group to my current ip: $IP";
 aws ec2 authorize-security-group-ingress --group-id $SECURITY_GROUP_ID --protocol tcp --port 22 --cidr $IP/32 --output text
 
-echo "Waiting to get the new dns name for the instance"
+# Return the dns name of the ec2 instance
+echo "Waiting to get the new dns name for the instance";
 sleep 20
-public_dns=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query Reservations[0].Instances[0].NetworkInterfaces[].Association.PublicDnsName)
-echo "The dns name is: $public_dns"
+public_dns=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --output text --query Reservations[0].Instances[0].NetworkInterfaces[].Association.PublicDnsName)
+echo "The dns name is: $public_dns";
+
+# Update the vscode config with the dns name of the ec2 instance
+echo "Updating the vscode config with the new dns name";
+cat > $VSCODE_SSH_CONFIG_PATH <<EOL
+Host $public_dns
+  HostName $public_dns
+  User ec2-user
+  IdentityFile $VSCODE_SSH_PEM_FILE_PATH
+EOL
+
+# Connect via ssh client to the ec2 host
+echo "Connecting to the ec2 host";
+ssh -i $SSH_PEM_FILE_PATH ec2-user@$public_dns
+/
